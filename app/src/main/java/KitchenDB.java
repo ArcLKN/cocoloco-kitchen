838e6ba1b66d42a4f2656f26fe0e69dc2e98fab2
ArcLKN <kalidolkn@gmail.com>
2025-05-03 02:45:27 +0200

    Fixed error with ide recommandation

--- app/src/main/java/KitchenDB.java
+++ app/src/main/java/KitchenDB.java
@@ -2,11 +2,17 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import androidx.annotation.Nullable;
+
 public class KitchenDB extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "kitchen.db";
     private static final int DATABASE_VERSION = 1;
 
     public static final String TABLE_NAME = "";
+
+    public KitchenDB(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
